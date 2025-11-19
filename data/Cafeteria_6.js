8d26f2ed0397ff4f89f168cfb5b35b1bb4d00771
rodriveras <153325533+rodriveras@users.noreply.github.com>
2025-11-19 14:24:34 -0300

    Add files via upload

---
+++ data/Cafeteria_6.js
@@ -0,0 +1,9 @@
+var json_Cafeteria_6 = {
+"type": "FeatureCollection",
+"name": "Cafeteria_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Cafetería Cuna de Parra" }, "geometry": { "type": "Point", "coordinates": [ -71.548840528179653, -36.559637591435568 ] } },
+{ "type": "Feature", "properties": { "name": "Cafetería Canela Pasión" }, "geometry": { "type": "Point", "coordinates": [ -71.548414414451315, -36.556207408179695 ] } }
+]
+}
